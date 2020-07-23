110fb4fc5dcd35fccaece71f27d1ba054259adca
geb <853934146@qq.com>
2020-07-23 19:32:16 +0800

    update

--- read_wav.cc
+++ read_wav.cc
@@ -1,17 +1,36 @@
+#include <cstring>
 #include <fstream>
-#include <vector>
 #include <iostream>
 #include <string>
-#include <cstring>
-using namespace  std;
+#include <vector>
 
-int main(int argc, char const *argv[])
+typedef std::pair<int, bool> riff_chunk;
+
+riff_chunk _read_riff_chunk(std::ifstream& fid)
 {
-    std::ifstream file("OSR_us_000_0010_8k.wav", ios::in | ios::binary);
+    bool is_big_endian;
+    int file_size;
     char str1[4];
-    file.read(str1, 4);
-    if(strcmp(str1, "strcmp")){
-        cout << str1 << endl;
+    fid.read(str1, 4);
+    if (strcmp(str1, "RIFF")) {
+        is_big_endian = false;
+    } else if (strcmp(str1, "RIFX")) {
+        is_big_endian = true;
+    } else {
+        throw std::invalid_argument("File format " + std::string(str1) + "... not understood.");
     }
+    fid.read((char*)&file_size, 4);
+    file_size += 8;
+    return std::make_pair(file_size, is_big_endian);
+}
+
+
+int main(int argc, char const* argv[])
+{
+    std::ifstream file("OSR_us_000_0010_8k.wav", std::ios::in | std::ios::binary);
+    
+    riff_chunk rc = _read_riff_chunk(file);
+    
+    std::cout << rc.first << "," << rc.second << std::endl;
     return 0;
 }
